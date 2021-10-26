7197263986e111519500aa6424c87ba7928eac47
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-25 21:56:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let base:number=document.getElementById("base");
+
+let exponente:number=document.getElementById("exponente");
+
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  function base(base:number=1) {
+  while (base>0) {
+    let basepor=(base*base);
+
+    return basepor;
+  }
+}
+ console.log(basepor);
+
